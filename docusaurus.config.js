3775e234d631481175e3e338b7f4f851f67ba3e9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-12-30 00:25:58 +0530

    helpsite: Fix labs broken path
    Change-Id: Icb71f2e3137b34ffc1928c60a0ba2a91862afa9a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
             position: "left",
             label: "Courses",
           },
-          { to: "/docs/lab", label: "Labs", position: "left" },
+          { to: "/docs/labs", label: "Labs", position: "left" },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: "https://github.com/UtsavBalar1231",
